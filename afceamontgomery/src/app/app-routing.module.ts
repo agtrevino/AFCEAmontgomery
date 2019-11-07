34dbca0e5d5d317177b7afc270fbf389b86bbbcd
Aaron Trevino <agtrevino91@yahoo.com>
2019-11-07 14:18:51 -0600

    updated upcoming courses

--- afceamontgomery/src/app/app-routing.module.ts
+++ afceamontgomery/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UpcomingCoursesComponent } from './upcoming-courses/upcoming-courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'upcoming-courses',
+    component: UpcomingCoursesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- afceamontgomery/src/app/app.module.ts
+++ afceamontgomery/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { UpcomingCoursesComponent } from './upcoming-courses/upcoming-courses.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './footer/footer.component';
     HomePageComponent,
     MainNavComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    UpcomingCoursesComponent
   ],
   imports: [
     BrowserModule,
